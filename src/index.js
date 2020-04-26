157495b6c342389ae603c7a5d9f02105dbf5b23a
Shinde Monali Ajinath <vp.monali@gmail.com>
2020-04-26 16:35:43 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,69 @@
+
+const app = require('express')();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const connection = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password: 'password',
+	database: 'test'
+});
+
+connection.connect((err) => {
+	if (err) {
+		console.error('error connecting mysql: ');
+	} else {
+		console.log('mysql connection successful');
+		}
+	
+});
+
+// home page
+app.get('/', (req, res) => {
+	res.json({
+		success: true,
+		message: 'Hello world'
+	});
+});
+
+// insert a student into database
+app.post('/', (req, res) => {
+	const student = req.body;
+	const query = 'INSERT INTO students values(?, ?, ?, ?, ?)';
+
+	connection.query(query, [student.ID, student.Name,student.Age,student.Department,student.Subject], (err, results, fields) => {
+		if (err) {
+			console.error(err);
+			res.json({
+				success: false,
+				message: 'Error occured'
+			});
+		} else {
+			res.json({
+				success: true,
+				message: 'Successfully added student'
+			});
+		}
+	});
+});
+
+// fetch all students
+app.post('/', (req, res) => {
+	const query = 'SELECT * FROM students';
+    connection.query(query, (err, results, fields) => {
+    	if (err) {
+    		console.error(err);
+    		res.json({
+    			success: false,
+    			message: 'Error occured'
+    		});
+    	} else {
+    		res.json({
+    			success: true,
+    			result: results
+    		});
+    	}
+    });
+});
+
