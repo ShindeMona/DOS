bafd6b2ca38d51982f0f46647c21c4ecf225971c
Shinde Monali Ajinath <vp.monali@gmail.com>
2020-04-27 20:23:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
 const app = require('express')();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
@@ -29,27 +29,28 @@ app.get('/', (req, res) => {
 
 // insert a student into database
 app.post('/user', (req, res) => {
-	const student = req.body;
-	const query = 'INSERT INTO students values(?, ?, ?, ?, ?)';
+       const id=req.body.id;
+       const Name=req.body.Name;
+       const Age=req.body.Age;
+       const Department=req.body.Department;
+       const Subject=req.body.Subject;
+       const query = 'INSERT INTO students values(? ,? , ?, ?, ?)';
 
-	connection.query(query, [student.ID, student.Name,student.Age,student.Department,student.Subject], (err, results, fields) => {
-		if (err) {
-			console.error(err);
-			res.json({
-				success: false,
-				message: 'Error occured'
-			});
-		} else {
-			res.json({
-				success: true,
-				message: 'Successfully added student'
-			});
-		}
-	});
+        connection.query(query, [id,Name,Age,Department,Subject], (err, results, fields) => {
+                if (err) {
+                        console.error(err);
+                        res.json({
+                                success: false,
+                                message: 'Error occured'
+                        });
+                } else {
+                res.send(results);
+                }
+        });
 });
 
 // fetch all students
-app.post('/all', (req, res) => {
+app.get('/user', (req, res) => {
 	const query = 'SELECT * FROM students';
     connection.query(query, (err, results, fields) => {
     	if (err) {
@@ -59,13 +60,79 @@ app.post('/all', (req, res) => {
     			message: 'Error occured'
     		});
     	} else {
+    		res.send(results);
+    	}
+    });
+});
+ 
+//fetch one student
+
+app.get('/user/:id', (req, res) => {
+	const query = 'SELECT * FROM students where id= ?';
+    connection.query(query,[req.params.id], (err, results, fields) => {
+    	if (err) {
+    		console.error(err);
+    		res.json({
+    			success: false,
+    			message: 'Error occured'
+    		});
+    	} else {
     		res.json({
     			success: true,
+                        message: 'Got record',
     			result: results
     		});
     	}
     });
 });
- app.listen(8080, () => {
+
+
+
+app.delete('/user/:id', (req, res) => {
+	const query = 'DELETE FROM students where id=?';
+    connection.query(query,[req.params.id], (err, results, fields) => {
+    	if (err) {
+    		console.error(err);
+    		res.json({
+    			success: false,
+    			message: 'Error occured'
+    		});
+    	} else {
+    		res.json({
+    			success: true,
+                        message: 'Deleted sucessfully',
+    			result: results
+    		});
+    	}
+    });
+});
+
+//Update
+
+
+
+app.put('/user/:id', (req, res) => {
+       const id=req.params.id;
+       const Name=req.body.Name;
+       const Age=req.body.Age;
+       const Department=req.body.Department;
+       const Subject=req.body.Subject;
+       const query = 'UPDATE students SET Name= ?,Age= ?,Department= ?,Subject= ? WHERE id= ?';
+
+        connection.query(query, [ Name,Age,Department,Subject,id], (err, results, fields) => {
+                if (err) {
+                        console.error(err);
+                        res.json({
+                                success: false,
+                                message: 'Error occured'
+                        });
+                } else {
+                res.send(results);
+                }
+        });
+});
+
+
+app.listen(8080, () => {
     console.log("Server is listening on port 8080");
 });
