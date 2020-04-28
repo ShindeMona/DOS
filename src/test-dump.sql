1dced061be74a755636b5f653f1e136ce1952ee9
Shinde Monali Ajinath <vp.monali@gmail.com>
2020-04-29 00:48:23 +0530

    Update test-dump.sql

--- src/test-dump.sql
+++ src/test-dump.sql
@@ -1,17 +1,8 @@
 create database test;
 use test;
-CREATE TABLE students (
-  ID int(11) NOT NULL,
-  Name varchar(255) DEFAULT NULL,
-   Age int(11),
-   Department varchar(255),
-    Subject varchar(255)
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+create table students(id int primary key,Name varchar(30),Age int,Department varchar(30),Subject varchar(30));
 
 INSERT INTO students VALUES (101,'Monali Shinde',22,'Com.Sci','ADS,PDM,DOS,FM2,PRO');
-
 INSERT INTO students VALUES (102,'Monali Shinde',22,'Com.Sci','ADS,PDM,DOS,FM2,PRO');
 INSERT INTO students VALUES (103,'Vihan Shinde',22,'Com.Sci','ADS,OR,DOS,FM2,PRO');
 INSERT INTO students VALUES (104,'Navin Shinde',22,'Com.Sci','ADS,OR,DOS,FM2,PRO');
@@ -20,4 +11,4 @@ INSERT INTO students VALUES (106,'devraj nimbalkar',22,'Com.Sci','ADS,DML,DOS,FM
 INSERT INTO students VALUES (107,'Vidya Nimbalkar',22,'Com.Sci','ADS,PDM,DOS,FM2,PRO');
 INSERT INTO students VALUES (108,'Varsha Bhange',22,'Com.Sci','ADS,PDM,DOS,FM2,PRO');
 INSERT INTO students VALUES (110,'Monali Thakur',22,'Com.Sci','ADS,PDM,DOS,FM2,PRO');
-UNLOCK TABLES;
+
