15311715db938ba540ea4c5ffdf2bddc134a75e4
Shinde Monali Ajinath <vp.monali@gmail.com>
2020-04-29 13:50:26 +0530

    Update test-dump.sql

--- Docker/test-dump.sql
+++ Docker/test-dump.sql
@@ -1,6 +1,6 @@
-DROP DATABASE test1;
-CREATE DATABASE test1;
-USE test1;
+DROP DATABASE test;
+CREATE DATABASE test;
+USE test;
 CREATE TABLE students(id INT PRIMARY KEY,Name VARCHAR(30),Age INT,Department VARCHAR(30),Subject VARCHAR(30));
 
 INSERT INTO students VALUES(1,'monali',22,'Comp.Sci','ADS,PDM,DOS,FM2,PROJ');
